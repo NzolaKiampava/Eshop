0b0c45ad4a09758aa741d0fcf90c55e7615c3913
Nzola Kiampava <nzolakiampava@gmail.com>
2022-09-29 03:08:04 -1200

    Update functions.php

--- app/core/functions.php
+++ app/core/functions.php
@@ -20,3 +20,21 @@ function esc($data)
 {
 	return addslashes($data);
 }
+
+function redirect($link)
+{
+	header("Location: " . ROOT . $link);
+	die;
+}
+
+
+//return a product string name to url version name link. ex.: https:public/home/product/product-string-url
+function str_to_url($url) {                     
+	$url = preg_replace('~[^\\pL0-9_]+~u', '-', $url);
+	$url = trim($url, "-");
+	$url = iconv("utf-8", "us-ascii//TRANSLIT", $url);
+	$url = strtolower($url);
+	$url = preg_replace('~[^-a-z0-9_]+~', '', $url);
+	return $url;
+}
+
