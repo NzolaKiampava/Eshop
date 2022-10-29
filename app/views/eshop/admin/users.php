9a8a4826b7a7a8e1e5ffb9102d9f61e34fb2397a
Nzola Kiampava <nzolakiampava@gmail.com>
2022-10-29 09:27:07 -0300

    Update users.php

--- app/views/eshop/admin/users.php
+++ app/views/eshop/admin/users.php
@@ -3,31 +3,69 @@
 
 <div class="row mt">
 	<div class="col-md-12">
-		<div class="content-panel table-responsive">
-			<table class="table table-striped table-advance table-hover">
-				<thead>
+		<div class="content-panel table-responsive" style="padding: 10px;">
+		
+		<span style="cursor: pointer;"  data-toggle="modal" data-target="#myModal" style="color: white;"><button type="button" class="btn btn-primary btn-ls pull-left"><i class="fa fa-plus"></i> Add new User</button></span>
+
+		<table class="table table-striped table-advance table-hover">
+			<thead>
+				<tr>
+					<th>user id</th>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Date Created</th>
+					<th>Ordes count</th>
+				</tr>
+			</thead>
+			<tbody onclick="show_detail(event)">
+			<?php if (is_array($users)): ?>
+				<?php foreach ($users as $user): ?>
 					<tr>
-						<th>user id</th>
-						<th>Name</th>
-						<th>Email</th>
-						<th>Date Created</th>
-						<th>Ordes count</th>
+						<td><?=$user->id?></td>
+						<td><a href="<?=ROOT?>profile/<?=$user->url_address?>"><?=$user->name?></a></td>
+						<td><?=$user->email?></td>
+						<td><?=date("jS M Y H:i a", strtotime($user->date))?></td>
+						<td><?=$user->orders_count?></td>
 					</tr>
-				</thead>
-				<tbody onclick="show_detail(event)">
-				<?php if (is_array($users)): ?>
-					<?php foreach ($users as $user): ?>
-						<tr>
-							<td><?=$user->id?></td>
-							<td><a href="<?=ROOT?>profile/<?=$user->url_address?>"><?=$user->name?></a></td>
-							<td><?=$user->email?></td>
-							<td><?=date("jS M Y H:i a", strtotime($user->date))?></td>
-							<td><?=$user->orders_count?></td>
-						</tr>
-					<?php endforeach; ?>
-				<?php endif; ?>
-				</tbody>
-			</table>
+				<?php endforeach; ?>
+			<?php endif; ?>
+			</tbody>
+		</table>
+		
+		<!-- Modal -->
+		
+          <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                          <h4 class="modal-title">Add new User</h4>
+                      </div>
+                      <div class="modal-body">
+                      	<form method="post">
+                      	  <p>Enter with Username.</p>
+                          <input type="text" name="name" placeholder="Username" autocomplete="off" class="form-control placeholder-no-fix">
+                          <p>Enter with email</p>
+                          <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
+
+                          <p>Select a rank </p>
+                          <select name="rank" class="form-control placeholder-no-fix" autocomplete="off">
+                        	<option value="admin">Administrador</option>
+                        	<option value="customer" selected>Cliente</option>
+                      	  </select>
+
+	                      </div>
+	                      <div class="modal-footer">
+	                          <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
+	                          <button class="btn btn-theme" type="submit">Save</button>
+	                      </div>
+                  		</form>
+                  </div>
+              </div>
+          </div>
+      	
+        <!-- modal -->
+
 		</div>
 	</div>
 </div>
