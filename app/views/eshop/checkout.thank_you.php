39eee9527e601b3dd14be65674c7db71a3e56760
Nzola Kiampava <nzolakiampava@gmail.com>
2022-11-02 13:20:55 -0300

    Update checkout.thank_you.php

--- app/views/eshop/checkout.thank_you.php
+++ app/views/eshop/checkout.thank_you.php
@@ -1,7 +1,7 @@
 <?php $this->view("header", $data); ?>
 <div style="text-align: center;">
 	
-		<center><img width="200" src="<?=ASSETS . THEME?>nearby-business.png"></center>
+	<center><img width="200" src="<?=ASSETS . THEME?>nearby-business.png"></center>
 	
 	<h1>Thank you for Shopping with us!</h1>
 	<h4 style="color: green;">Your order was sucessful</h4>
@@ -16,5 +16,4 @@
 	<input type="button" class="btn btn-warning" value="View your orders">
 	</a><br><br>
 </div>
-
 <?php $this->view("footer", $data); ?>
