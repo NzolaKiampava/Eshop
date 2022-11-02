f1c386df8c5fb94b3cfe6781eef558a599b12b19
Nzola Kiampava <nzolakiampava@gmail.com>
2022-11-02 12:54:45 -0300

    Update sidebar.php

--- app/views/eshop/admin/sidebar.php
+++ app/views/eshop/admin/sidebar.php
@@ -2,12 +2,19 @@
 MAIN SIDEBAR MENU
 *********************************************************************************************************************************************************** -->
 <!--sidebar start-->
+<?php
+   $image = ROOT . 'uploads/default-user.jpg';
+   if (file_exists($user_data->image)) //looking for if exist some file int the collumn image
+   {
+        $image = $user_data->image;
+   }
+?>
 <aside>
     <div id="sidebar"  class="nav-collapse ">
         <!-- sidebar menu start-->
         <ul class="sidebar-menu" id="nav-accordion">
         
-              <p class="centered"><a href="profile.html"><img src="<?=ASSETS . THEME?>admin/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
+              <p class="centered"><a href="<?=ROOT?>admin/admin_profile"><img src="<?=ROOT.$image?>" class="img-circle" width="60"></a></p>
               <h5 class="centered"><?=$data['user_data']->name;?></h5>
               <h5 class="centered" style="font-size:11px;"><?=$data['user_data']->email;?></h5>
 
