e8be5907c1d90bbd2ef5062729d3df45db3edc40
Nzola Kiampava <nzolakiampava@gmail.com>
2022-12-06 23:51:13 -1200

    Update admin.php

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -2,393 +2,336 @@
 
 Class Admin extends Controller
 {
+
 	public function index()
 	{
 		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
+		$user_data = $User->check_login(true, ["admin"]);
 
 		if(is_object($user_data)){
 			$data['user_data'] = $user_data;
 		}
-		$DB = Database::newInstance();
-		$data['users'] = $DB->read("select * from users");
-		$data['groups'] = $DB->read("SELECT * FROM colector_group");
-		$data['limit_users'] = $DB->read("select * from users order by id desc limit 8");
-
-		$data['count_trash'] = $DB->read('SELECT * FROM trash_buckets');
-		$data['count_trash_full'] = $DB->read("SELECT * FROM trash_buckets where status = 'full'");
-		$data['count_trash_empty'] = $DB->read("SELECT * FROM trash_buckets where status = 'empty'");
-
-		$data['count_address'] = $DB->read("SELECT * FROM garbage_address");	
-		$data['count_car'] = $DB->read("select * from garbage_cars");
 
 		$data['page_title'] = "Admin";
+		$data['current_page'] = "dashboard";
 		$this->view("admin/index", $data);
 	}
 
-//profile for admins and other previlegies users
-	public function profile()
+	public function categories()
 	{
 		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
+		$user_data = $User->check_login(true, ["admin"]);
 
 		if(is_object($user_data)){
 			$data['user_data'] = $user_data;
 		}
-		$DB = Database::newInstance();
-		$data['users'] = $DB->read("select * from users");
 
-		$data['count_trash'] = $DB->read('SELECT * FROM trash_buckets');
-		$data['count_trash_full'] = $DB->read("SELECT * FROM trash_buckets where status = 'full'");
-		$data['count_trash_empty'] = $DB->read("SELECT * FROM trash_buckets where status = 'empty'");
+	    $DB = Database::newInstance();
+	    $categories_all = $DB->read("select * from categories order by id desc");
+	    $categories = $DB->read("select * from categories where disabled = 0 order by id desc");
+	    
 
-		//verify user created
-		$session_user = $_SESSION['user_url'];
-		$create_check = $DB->read("SELECT * FROM users WHERE url_address = '$session_user' limit 1");
-		$user_created = $create_check[0]->id;
+	    $category = $this->load_model('Category');
+	    $tbl_rows = $category->make_table($categories_all);
+	    $data['tbl_rows'] = $tbl_rows;
+	    $data['categories'] = $categories;
 
-		$data['user_created'] = $DB->read("select * from users where created_by = '$user_created'");
+		$data['page_title'] = "Admin - Categories";
+		$data['current_page'] = "categories";
+		$this->view("admin/categories", $data);
+	}
 
-		$id = $data['user_data']->id;
+	public function products()
+	{
+		$User = $this->load_model('User');
+		$user_data = $User->check_login(true, ["admin"]);
 
-		if(isset($_POST['admin_profile_button']))
+		if(is_object($user_data)){
+			$data['user_data'] = $user_data;
+		}
+
+	    $DB = Database::newInstance();
+	    $products = $DB->read("select * from products order by id desc");
+
+	    $categories = $DB->read("select * from categories where disabled = 0 order by id desc");
+
+	    //show($categories);
+	    $product = $this->load_model('Product');
+	    $category = $this->load_model('Category');
+
+	    $tbl_rows = $product->make_table($products,$category);
+	    $data['tbl_rows'] = $tbl_rows;
+	    $data['categories'] = $categories;
+
+		$data['page_title'] = "Admin - Products";
+		$data['current_page'] = "products";
+		$this->view("admin/products", $data);
+	}
+
+	public function orders()
+	{
+		$User = $this->load_model('User');
+		$Order = $this->load_model('Order');
+		$user_data = $User->check_login(true, ["admin"]);
+
+		$orders = $Order->get_all_oders();
+
+		if (is_array($orders)) {
+			foreach ($orders as $key => $row) {
+				// code...
+				$details = $Order->get_order_details($row->id);
+				$orders[$key]->grand_total = 0;
+				if(is_array($details))
+				{
+					$totals = array_column($details, "total"); //get all item with specific total value
+					$grand_total = array_sum($totals);  //sum the totals
+					$orders[$key]->grand_total = $grand_total;
+
+				}
+				
+				$orders[$key]->details = $details;
+				$orders[$key]->user = $User->get_user($row->user_url);
+				$orders[$key]->grand_total = $grand_total;
+			}
+		}
+
+		if(is_object($user_data)){
+			$data['user_data'] = $user_data;
+		}
+		$data['page_title'] = "Admin - Orders";
+		$data['orders'] = $orders;
+		$data['current_page'] = "orders";
+		$this->view("admin/Orders", $data);
+	}
+
+	public function users($type = "customers")
+	{
+		$User = $this->load_model('User');
+		$Order = $this->load_model('Order');
+
+		$user_data = $User->check_login(true, ["admin"]);
+
+		if(is_object($user_data)){
+			$data['user_data'] = $user_data;
+		}
+		if($type == "admins")
 		{
-			//show($_POST);
-			$User->update_admin_profile($_POST, $id);
+			$users = $User->get_admins();
+		}else{
+			$users = $User->get_customers();
+		}
+
+		if(is_array($users)){
+			foreach ($users as $key => $row) {
+				// code...
+				$orders_num = $Order->get_orders_count($row->url_address);
+				$users[$key]->orders_count = $orders_num;
+			}
+		}
+
+		$data['users'] = $users;
+		$data['page_title'] = "Admin - $type";
+		$data['current_page'] = "users";
+		$this->view("admin/users", $data);
+
+	}
+
+	public function settings($type = '')
+	{
+		$User = $this->load_model('User');
+		$Settings = new Settings();
+
+		$user_data = $User->check_login(true, ["admin"]);
+		if(is_object($user_data)){
+			$data['user_data'] = $user_data;
+		}
+
+		//select the right page
+		if($type == 'socials'){
+
+			if(count($_POST) > 0)
+			{
+				$error = $Settings->save_settings($_POST);
+				header("Location: " . ROOT . "admin/settings/socials");
+				die;
+			}
+
+		}else
+		if($type == "slider_images"){
+
+			$data['action'] = "show";				
+			$Slider = $this->load_model('Slider');
+			
+			//read all slider image
+			$data['rows'] = $Slider->get_all();
+
+			if(isset($_GET['action']) && $_GET['action'] == "add"){
+				$data['action'] = "add";
+
+				//if new row was posted
+
+				if(count($_POST) > 0)
+				{
+					//show($_POST);
+					//show($_FILES);
+					$Image = $this->load_model('Image');
+					$data['errors'] = $Slider->create($_POST, $_FILES, $Image);
+					//show($data['errors']);
+					$data['POST'] = $_POST;
+					header("Location: " . ROOT . "admin/settings/slider_images");
+					die;
+				}
+
+			}else
+			if(isset($_GET['action']) && $_GET['action'] == "edit"){
+				$data['action'] = "edit";
+				$data['id'] = null;
+				if(isset($_GET['id'])){
+					$data['id'] = $_GET['id'];
+				}
+
+			}else
+			if(isset($_GET['action']) && $_GET['action'] == "delete"){
+
+			}
+			else
+			if(isset($_GET['action']) && $_GET['action'] == "delete_comfirmed"){
+
+			}
+			
+		}
+
+		$data['settings'] = $Settings->get_all_settings();
+		$data['type'] = $type;
+		$data['page_title'] = "Admin - $type";
+		$data['current_page'] = "settings";
+		$this->view("admin/settings", $data);
+	}
+
+	public function messages($type = '')
+	{
+		$type = "Messages";
+		$mode = "read";
+
+		$User = $this->load_model('User');
+		$Message = $this->load_model('Message');
+
+		$user_data = $User->check_login(true, ["admin"]);
+
+		if(is_object($user_data)){
+			$data['user_data'] = $user_data;
+		}
+		
+		if(isset($_GET['delete']))
+		{
+			$mode = "delete";
+		} 
+
+		if(isset($_GET['delete_confirmed']))
+		{
+			$mode = "delete_confirmed";
+			$id = $_GET['delete_confirmed'];
+			$Message->delete($id);
+		}
+		
+
+		if ($mode == "delete") {
+			$id = $_GET['delete'];
+			$messages = $Message->get_one($id);
+		}else{
+			$messages = $Message->get_all();
+		}
+		
+		$data['mode'] = $mode;
+		$data['messages'] = $messages;
+		$data['page_title'] = "Admin - $type";
+		$data['current_page'] = "messages";
+		$this->view("admin/messages", $data);
+
+	}
+
+	public function blogs($type = '')
+	{
+		$type = "Blog Posts";
+		$mode = "read";
+
+		$User = $this->load_model('User');
+		$post_class = $this->load_model('post');
+
+		$user_data = $User->check_login(true, ["admin"]);
+
+		if(is_object($user_data)){
+			$data['user_data'] = $user_data;
+		}
+		
+		if(isset($_GET['add_new']))
+		{
+			$mode = "add_new";
+		} 
+
+		if(isset($_GET['edit']))
+		{
+			$mode = "edit";
+		} 
+
+		if(isset($_GET['delete']))
+		{
+			$mode = "delete";
+		} 
+		
+		if(isset($_GET['delete_confirmed']))
+		{
+			$mode = "delete_confirmed";
+			$id = $_GET['delete_confirmed'];
+			$post_class->delete($id);
+		}
+		
+		if ($mode == "edit") {
+			$id = $_GET['edit'];
+			$blogs = $post_class->get_one($id);
+			$data['POST'] = (array)$blogs;
+		}
+		else
+			if ($mode == "delete") {
+			$id = $_GET['delete'];
+			$blogs = $post_class->get_one($id);
+		}else{
+			$blogs = $post_class->get_all();
+			$image_class = $this->load_model('Image');
+
+			if ($blogs) {
+				foreach ($blogs as $key => $row) {
+					// code...
+					if(file_exists($blogs[$key]->image)){
+						$blogs[$key]->image = $image_class->get_thumb_post($blogs[$key]->image);
+					}
+					// find the user by his user_url
+					$blogs[$key]->user_data = $User->get_user($blogs[$key]->user_url);
+				}
+			}
+		}
+
+		//if something was posted
+		if(count($_POST) > 0)
+		{
+			$post = $this->load_model('post');
+			$image_class = $this->load_model('image');
+			$post_class->create($_POST,$_FILES,$image_class);
 
 			if(isset($_SESSION['error']) && $_SESSION['error'] != ""){
-				//show($_SESSION['error']);
 				$data['errors'] = $_SESSION['error'];
 				$data['POST'] = $_POST;
+			}else{
+				redirect("admin/blogs");
 			}
 		}
-
-		$data['page_title'] = "Profile";
-		$this->view("admin/profile", $data);
-	}
-
-	public function groups()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
-
-		$Group = $this->load_model('Group');
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-
-		//add group
-		if(isset($_POST['add_group']))
-		{
-			$Group->add_group($_POST);
-		}
-
-		//edit group
-		if(isset($_POST['edit_group']))
-		{
-			$Group->edit_group($_POST);
-		}
-
-		//delete group
-		if(isset($_POST['delete_group']))
-		{
-			$Group->delete_group($_POST);
-		}
-
-		$DB = Database::newInstance();
-
-		$data['count_trash'] = $DB->read('SELECT * FROM trash_buckets');
-		$data['count_trash_full'] = $DB->read("SELECT * FROM trash_buckets where status = 'full'");
-		$data['count_trash_empty'] = $DB->read("SELECT * FROM trash_buckets where status = 'empty'");
-
-		$data['users'] = $DB->read("select * from users order by id desc");
-		$data['groups'] = $DB->read("select * from colector_group order by id desc");
-		$data['page_title'] = "Groups";
-		$this->view("admin/groups", $data);
-	}
-
-	public function users()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
-
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-		$DB = Database::newInstance();
-
-		$data['count_trash'] = $DB->read('SELECT * FROM trash_buckets');
-		$data['count_trash_full'] = $DB->read("SELECT * FROM trash_buckets where status = 'full'");
-		$data['count_trash_empty'] = $DB->read("SELECT * FROM trash_buckets where status = 'empty'");
-
-		$data['users'] = $DB->read("select * from users order by id desc");
-
-		// add user
-		if(isset($_POST['add']))
-		{
-			//show($_POST);
-			$User->add_user($_POST);
-		}
-
-		//edit user
-		if(isset($_POST['edit']))
-		{
-			//show($_POST);
-			$User->edit_user($_POST);
-		}
-
-		//delete user
-		if(isset($_POST['delete']))
-		{
-			$User->delete_user($_POST);
-		}
-
-		//upload_photo
-		if(isset($_POST['upload_photo']))
-		{
-			$User->upload_photo($_POST);
-		}
-
-		$data['page_title'] = "Usuarios";
-		$this->view("admin/users", $data);
-	}
-
-	public function trash()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
-
-		$trash = $this->load_model('Trash');
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-
-		//add trash
-		if(isset($_POST['add_trash']))
-		{
-			$trash->add_trash($_POST);
-		}
-
-		//edit trash
-		if(isset($_POST['edit_trash']))
-		{
-			$trash->edit_trash($_POST);
-		}
-
-		//delete trash
-		if(isset($_POST['delete_trash']))
-		{
-			$trash->delete_trash($_POST);
-		}
-
-		$DB = Database::newInstance();
-
-		$data['count_trash'] = $DB->read('SELECT * FROM trash_buckets');
-		$data['count_trash_full'] = $DB->read("SELECT * FROM trash_buckets where status = 'full'");
-		$data['count_trash_empty'] = $DB->read("SELECT * FROM trash_buckets where status = 'empty'");
-
-		$data['users'] = $DB->read("select * from users order by id desc");
-		$data['trashes'] = $DB->read("select * from trash_buckets order by id desc");
-		$data['page_title'] = "Trash";
-		$this->view("admin/trash", $data);
-	}
-
-	public function address()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
-
-		$address = $this->load_model('Address');
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-
-		//add address
-		if(isset($_POST['add_address']))
-		{
-			$address->add_address($_POST);
-		}
-
-		//edit address
-		if(isset($_POST['edit_address']))
-		{
-			$address->edit_address($_POST);
-		}
-
-		//delete address
-		if(isset($_POST['delete_address']))
-		{
-			$address->delete_address($_POST);
-		}
-
-		$DB = Database::newInstance();
-
-		$data['count_trash'] = $DB->read('SELECT * FROM trash_buckets');
-		$data['count_trash_full'] = $DB->read("SELECT * FROM trash_buckets where status = 'full'");
-		$data['count_trash_empty'] = $DB->read("SELECT * FROM trash_buckets where status = 'empty'");
-
-		$data['users'] = $DB->read("select * from users order by id desc");
-		$data['address'] = $DB->read("select * from garbage_address order by id desc");
-		$data['page_title'] = "Address";
-		$this->view("admin/address", $data);
-	}
-
-	//colector cars
-	public function trucks()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
 		
-		$truck = $this->load_model('Truck');
+		$data['mode'] = $mode;
+		$data['blogs'] = $blogs;
+		$data['page_title'] = "Admin - $type";
+		$data['current_page'] = "blogs";
+		$this->view("admin/blogs", $data);
 
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-		$DB = Database::newInstance();
-
-		if(isset($_POST['add']))
-		{
-			$truck->add_truck($_POST);
-		}
-
-		if(isset($_POST['edit']))
-		{
-			$truck->edit_truck($_POST);
-		}
-
-		if(isset($_POST['delete']))
-		{
-			$truck->delete_truck($_POST);
-		}
-		
-		$data['count_trash'] = $DB->read('SELECT * FROM trash_buckets');
-		$data['count_trash_full'] = $DB->read("SELECT * FROM trash_buckets where status = 'full'");
-		$data['count_trash_empty'] = $DB->read("SELECT * FROM trash_buckets where status = 'empty'");
-
-		$data['users'] = $DB->read("select * from users order by id desc");
-		$data['trashes'] = $DB->read("select * from trash_buckets order by id desc");
-		$data['trucks'] = $DB->read("select * from garbage_cars order by id desc");
-		$data['page_title'] = "Truck";
-		$this->view("admin/trucks", $data);
 	}
 
-	public function truck_row()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
-
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-		$DB = Database::newInstance();
-
-		if(isset($_POST['id'])){
-			$id = $_POST['id'];
-
-			$row = $DB->read("SELECT * FROM garbage_cars WHERE id=:id",['id'=>$id]);
-
-			echo json_encode($row);
-		}
-	}
-	
-	// some row no controllers
-	public function address_row()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
-
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-		$DB = Database::newInstance();
-
-		if(isset($_POST['id'])){
-			$id = $_POST['id'];
-
-			$row = $DB->read("SELECT * FROM garbage_address WHERE id=:id",['id'=>$id]);
-
-			echo json_encode($row);
-		}
-	}
-
-	/*public function address_fetch()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
-
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-		$DB = Database::newInstance();
-		$fetch = $DB->read("SELECT * FROM garbage_address");
-
-		if(is_array($fetch))
-		{
-			foreach($fetch as $row)
-			{
-				$output .= "
-					<option value='".$row->id."' class='append_items'>".$row->address."</option>
-				";
-			}
-		}
-
-		echo json_encode($output);
-	}*/
-
-	public function users_row()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
-
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-		$DB = Database::newInstance();
-
-		if(isset($_POST['id'])){
-			$id = $_POST['id'];
-
-			$row = $DB->read("SELECT * FROM users WHERE id=:id",['id'=>$id]);
-
-			echo json_encode($row);
-		}
-	}
-
-	public function trash_row()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
-
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-		$DB = Database::newInstance();
-
-		if(isset($_POST['id'])){
-			$id = $_POST['id'];
-
-			$row = $DB->read("SELECT *, (SELECT address from garbage_address where id='$id') as add_name FROM trash_buckets WHERE id=:id",['id'=>$id]);
-
-			echo json_encode($row);
-		}
-	}
-
-	public function groups_row()
-	{
-		$User = $this->load_model('User');
-		$user_data = $User->check_login(true, ["Administrador"]);
-
-		if(is_object($user_data)){
-			$data['user_data'] = $user_data;
-		}
-		$DB = Database::newInstance();
-
-		if(isset($_POST['id'])){
-			$id = $_POST['id'];
-
-			$row = $DB->read("SELECT * FROM colector_group WHERE id=:id",['id'=>$id]);
-
-			echo json_encode($row);
-		}
-	}
 }
